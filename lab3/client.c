2be99a616ed292944cb6f9ac6a5df225a89d3403
Zhang  He <heherbert.zhang@mail.utoronto.ca>
2018-02-16 11:24:43 -0500

    use sendto and recvfrom

--- lab3/client.c
+++ lab3/client.c
@@ -6,7 +6,14 @@
 #include <sys/time.h>
 #include <stdlib.h>
 
-float getRTT(int sockfd, char* msg);
+float getRTT(int sockfd, char* msg, struct addrinfo* p);
+int fillBuff(char* buf, int size) {
+  int i = 0;
+  for(i = 0; i < size+1; i++){
+    buf[i] = 'a';
+  }
+  buf[size] = '\0';
+}
 
 int main(int argc, char *argv[])
 {
@@ -27,31 +34,50 @@ int main(int argc, char *argv[])
   sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
   
   // connect!
-  connect(sockfd, res->ai_addr, res->ai_addrlen);
+  //connect(sockfd, res->ai_addr, res->ai_addrlen);
 
-  float RTT1 = getRTT(sockfd, "hi"); // 2 bytes = 16 bits
+  int msg1_size = 1000;
+  char msg1[msg1_size+1];
+  fillBuff(msg1, msg1_size);
+  float RTT1 = getRTT(sockfd, msg1, res); // 2 bytes = 16 bits
   printf("RTT is %f\n", RTT1);
-  float RTT2 = getRTT(sockfd, "hihihihihihihihihihi"); // 20 bytes = 160 bits 
+  int msg2_size = 1050;
+  char msg2[msg2_size+1];
+  fillBuff(msg2, msg2_size);
+  float RTT2 = getRTT(sockfd, msg2, res); // 20 bytes = 160 bits 
   printf("RTT is %f\n", RTT2);
   // RTT / 2 = transmission time + propagation time
   // RTT2/2 - RTT1/2 = transmission time for 20-2 bytes = 144bits
   // transmission rate = 144bits / timespent
   // propatation delay = RTT/2 - transmission time
-  float R = 144.0 / ((float)(RTT2-RTT1)/2.0);
-  float t_prop = RTT1/2.0 - 16.0/R;
-  float t_prop2 = RTT2/2.0 - 160.0/R;
+  float R = (msg2_size-msg1_size)*8 / ((float)(RTT2-RTT1)/2.0);
+  float t_prop = RTT1/2.0 - msg1_size*8.0/R;
+  float t_prop2 = RTT2/2.0 - msg2_size*8.0/R;
   printf("R is %f, and t_prop1 is %f which should be same as t_prop2 %f\n", \
     R, t_prop, t_prop2);
 }
 
-float getRTT(int sockfd, char* msg) {
-  size_t BUFSIZE = 1000;
-  int bytes_sent, bytes_received;
+float getRTT(int sockfd, char* msg, struct addrinfo* p) {
+  size_t BUFSIZE = 5000;
+  int bytes_sent=0, bytes_received=0;
   struct timeval curtime, echotime;
   char buffer[BUFSIZE];
+
+  
+  // connectors address info
+  struct sockaddr_storage their_addr;
+
   gettimeofday(&curtime, NULL);
-  bytes_sent = send(sockfd, msg, strlen(msg), 0);
-  bytes_received = recv(sockfd, buffer, BUFSIZE, 0);
+  
+  // communicate with server
+  socklen_t addr_len;
+  addr_len = sizeof their_addr;
+  bytes_sent = sendto(sockfd, msg, strlen(msg), 0, p->ai_addr, p->ai_addrlen);
+  bytes_received = recvfrom(sockfd, buffer, BUFSIZE, 0, (struct sockaddr *)&their_addr, &addr_len);
+  printf("sent bytes:%d, recv %d\n", bytes_sent, bytes_received);
+  
+  //bytes_sent = send(sockfd, msg, strlen(msg), 0);
+  //bytes_received = recv(sockfd, buffer, BUFSIZE, 0);
   gettimeofday(&echotime, NULL);
   return (float)(echotime.tv_sec-curtime.tv_sec) + \
     (((float)(echotime.tv_usec-curtime.tv_usec))/1000000.0);
